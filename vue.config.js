3671b5e934e61cfb810b8c423b894cb7dea68b34
LAPTOP-D35EQNF3\86130 <1428388240@qq.com>
2022-10-15 10:13:25 +0800

    初始化项目：目录，css，别名，模块

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  transpileDependencies: true
+  transpileDependencies: true,
+  //配置webpack
+  configureWebpack: {
+    resolve: {
+      alias: {
+        //vue默认有一个别名@: 'src'
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
 }
